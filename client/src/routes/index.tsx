a1627fcc39f925af09dad7deb90dfb2c1c34ab68
Dustin Neighly <neighlyd@gmail.com>
2026-06-29 10:03:06 -0700

    feat: prefetch roles on initial page load

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,14 +1,31 @@
 import * as React from "react";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link as LinkWrapper } from "@tanstack/react-router";
+
+import { rolesQueryOptions } from "../api/roles";
+import { Heading, Link, Text } from "@radix-ui/themes";
+import { AppLink } from "../components/AppLink";
 
 export const Route = createFileRoute("/")({
   component: Home,
+  loader: ({ context }) => {
+    /**
+     * We are firing off a prefetch event for our roles, so they will be ready once we land anywhere.
+     *
+     * @see https://tanstack.com/router/latest/docs/integrations/query#prefetching-and-streaming
+     */
+    context.queryClient.fetchQuery(rolesQueryOptions);
+  },
 });
 
 function Home() {
   return (
     <div>
-      <h3>Welcome Home!</h3>
+      <Heading>Welcome!</Heading>
+      <Text as="p">Let's get started.</Text>
+      <Text as="p">
+        Please select either <AppLink to="/users">Users</AppLink> or{" "}
+        <AppLink to="/roles">Roles</AppLink> to get started
+      </Text>
     </div>
   );
 }
