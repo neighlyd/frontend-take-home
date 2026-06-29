9cc39f9ad16bb13cff0eae15967638ced50c744a
Dustin Neighly <neighlyd@gmail.com>
2026-06-29 11:31:32 -0700

    feat: add data loader to users

--- client/src/routes/users.tsx
+++ client/src/routes/users.tsx
@@ -1,9 +1,36 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useSuspenseQueries } from "@tanstack/react-query";
+
+import { formatUsersList, usersQueryOptions } from "../api/users";
+import { formatRoleMap, rolesQueryOptions } from "../api/roles";
+import { SearchBar } from "../components/SearchBar";
 
 export const Route = createFileRoute("/users")({
   component: RouteComponent,
+  loader: async ({ context: { queryClient } }) => {
+    await Promise.all([
+      queryClient.ensureQueryData(usersQueryOptions),
+      queryClient.ensureQueryData(rolesQueryOptions),
+    ]);
+  },
+  pendingComponent: () => {
+    return <SearchBar disabled type="user" />;
+  },
 });
 
 function RouteComponent() {
+  const usersList = useSuspenseQueries({
+    queries: [usersQueryOptions, rolesQueryOptions],
+    combine: ([usersRes, rolesRes]) => {
+      if (usersRes.status === "success" && rolesRes.status === "success") {
+        const roleMap = formatRoleMap(rolesRes.data);
+        const usersList = formatUsersList(usersRes.data, roleMap);
+        return usersList;
+      }
+    },
+  });
+
+  console.log("merged:", usersList);
+
   return <div>Hello "/users"!</div>;
 }
