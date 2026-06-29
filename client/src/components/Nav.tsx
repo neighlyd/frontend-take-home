8d02c17efbd027f924fc79eae49fa19ba42e7d06
Dustin Neighly <neighlyd@gmail.com>
2026-06-29 10:02:23 -0700

    chore: update active element for TabNav

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-import { Link } from "@tanstack/react-router";
+import { Link, useRouterState } from "@tanstack/react-router";
 import { TabNav } from "@radix-ui/themes";
 
 export const Nav = () => {
+  const currentPath = useRouterState({
+    select: (state) => state.location.pathname,
+  });
+
   return (
     <TabNav.Root>
-      <TabNav.Link asChild active>
+      <TabNav.Link asChild active={currentPath === "/users"}>
         <Link to="/users">Users</Link>
       </TabNav.Link>
-      <TabNav.Link asChild>
+      <TabNav.Link asChild active={currentPath === "/roles"}>
         <Link to="/roles">Roles</Link>
       </TabNav.Link>
     </TabNav.Root>
