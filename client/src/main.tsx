ae3f0014f95ca5d224977e6a225040d0d0832c75
Dustin Neighly <neighlyd@gmail.com>
2026-06-29 09:21:23 -0700

    chore: remove body margin

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@radix-ui/themes/styles.css";
+import "./reset.css";
 
 import { routeTree } from "./routeTree.gen";
 import { Theme } from "@radix-ui/themes";
